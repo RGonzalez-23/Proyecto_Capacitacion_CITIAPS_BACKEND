ee7970f4f10c4cdb7a969cede99558da9256003e
Raul <raul.gonzalez.v@usach.cl>
2026-04-08 01:09:09 -0400

    Initialized backend estructure

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"backend/controller"
+	"backend/util"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	// Load .env file
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Println("Warning: Error loading .env file, using system environment variables")
+	}
+
+	// Load environment variables
+	mongoURI := os.Getenv("MONGO_URI")
+	dbName := os.Getenv("DB_NAME")
+
+	if mongoURI == "" || dbName == "" {
+		log.Fatalf("Missing environment variables: MONGO_URI or DB_NAME")
+	}
+
+	util.ConnectDB(mongoURI, dbName)
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/tasks", controller.CreateTask).Methods("POST")
+	r.HandleFunc("/tasks", controller.GetTasks).Methods("GET")
+	r.HandleFunc("/tasks/{id}", controller.GetTaskByID).Methods("GET")
+	r.HandleFunc("/tasks/{id}/complete", controller.CompleteTask).Methods("PUT")
+	r.HandleFunc("/tasks/{id}", controller.DeleteTask).Methods("DELETE")
+
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatalf("Could not start server: %s\n", err.Error())
+	}
+}
---
+++ util/database.go
@@ -0,0 +1,27 @@
+package util
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var DB *mongo.Database
+
+func ConnectDB(uri, dbName string) {
+	clientOptions := options.Client().ApplyURI(uri)
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatalf("Failed to connect to MongoDB: %v", err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatalf("Failed to ping MongoDB: %v", err)
+	}
+
+	DB = client.Database(dbName)
+	log.Println("Connected to MongoDB!")
+}
